ab94da0faa03ba343ed3f959add7d40259ccb96f
Osmond <osmondarnesto@gmail.com>
2021-01-04 20:05:44 -0500

    Add select menu to newMovieForm component, export movies array from fakeMovieService

--- src/components/newMovieForm.jsx
+++ src/components/newMovieForm.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Joi from 'joi-browser';
 import Form from './common/form';
+import { movies, saveMovie } from '../services/fakeMovieService';
+import { getGenres } from '../services/fakeGenreService';
 
 class NewMovieForm extends Form {
   state = {
     data: {
       title: '',
+      genre: '',
       numberInStock: '',
       dailyRentalRate: ''
     },
@@ -14,23 +17,51 @@ class NewMovieForm extends Form {
 
   schema = {
     title: Joi.string().required().label('Title'),
+    genre: Joi.string().required().label('Genre'),
     numberInStock: Joi.number().integer().min(0).max(100).required().label('Stock'),
     dailyRentalRate: Joi.number().required().min(0).max(10).label('Rate')
   };
 
-  doSubmit = () => {
-    // To be added to list of movies
-    console.log("Movie added!");
+  handleSelect = e => {
+    const data = {...this.state.data};
+    data.genre = e.target.value;
+
+    this.setState({data});
   };
 
-  render() { 
+  doSubmit = () => {
+    // To be added to list of movies
+    console.log(this.state.data);
+  };
+
+  render() {
+    const options = getGenres().map(genre => (
+      {
+        label: genre.name,
+        value: genre.name
+      }
+    ));
+
     return (
-      <div class='container'>
+      <div className='container'>
         <h1>Add a New Movie</h1>
         <form onSubmit={this.handleSubmit}>
           {this.renderInput('title', 'Title')}
-          
-          {/* select menu for genre here */}
+
+          <div className='form-group'>
+            <label htmlFor='genre'>Genre</label>
+            <select
+              name='genre'
+              id='genre'
+              className='custom-select'
+              onChange={this.handleSelect}
+            >
+              <option value=''></option>
+              {options.map(option => (
+                <option key={option.value} value={option.value}>{option.label}</option>
+              ))}
+            </select>
+          </div>
 
           {this.renderInput('numberInStock', 'Stock', 'number')}
           {this.renderInput('dailyRentalRate', 'Rate', 'number')}
