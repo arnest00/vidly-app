a28096acc1a192ad8512f87f1818360a977b24a6
Osmond <osmondarnesto@gmail.com>
2021-01-13 15:28:24 -0500

    Create config file, delete unneeded fake services, finish genre and movie services

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,30 +1,30 @@
 import http from './httpService';
+import { apiUrl } from '../config.json';
 
-const apiEndpoint = 'http://localhost:3900/api/movies';
+const apiEndpoint = apiUrl + '/movies';
+
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
 
 export async function getMovies() {
   return http.get(apiEndpoint);
 };
 
-// export function getMovie(id) {
-//   return movies.find(m => m._id === id);
-// };
+export async function getMovie(id) {
+  return http.get(movieUrl(id));
+};
 
-// export function saveMovie(movie) {
-//   let movieInDb = movies.find(m => m._id === movie._id) || {};
-//   movieInDb.title = movie.title;
-//   movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
-//   movieInDb.numberInStock = movie.numberInStock;
-//   movieInDb.dailyRentalRate = movie.dailyRentalRate;
-
-//   if (!movieInDb._id) {
-//     movieInDb._id = (Date.now()).toString();
-//     movies.push(movieInDb);
-//   };
-
-//   return movieInDb;
-// };
+export async function saveMovie(movie) {
+  if (movie._id) {
+    const body = {...movie};
+    delete body._id;
+    return http.put(movieUrl(movie._id), body);
+  }
+    
+  return http.post(apiEndpoint, movie);
+};
 
 export function deleteMovie(id) {
-  return http.delete(apiEndpoint + '/' + id);
+  return http.delete(movieUrl(id));
 };
