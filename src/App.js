b02526504dcbde44453c6f000dae394e6fe8cad1
Osmond <osmondarnesto@gmail.com>
2021-01-14 15:39:13 -0500

    Create user and auth services; create common logout component; implement user register, login, and logout with course-provided back-end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { toast, ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import jwtDecode from 'jwt-decode';
 import Movies from './components/movies';
 import MovieForm from './components/movieForm';
 import Navbar from './components/navbar';
@@ -8,17 +9,31 @@ import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import LoginForm from './components/common/loginForm';
+import Logout from './components/common/logout';
 import RegisterForm from './components/common/registerForm';
 import 'react-toastify/dist/ReactToastify.css';
 
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem('token');
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (ex) {};
+  };
+
   render() { 
     return (
       <React.Fragment>
         <ToastContainer />
-        <Navbar />
+        <Navbar 
+          user={this.state.user}
+        />
         <main className='container'>
           <Switch>
+            <Route path='/logout' component={Logout} />
             <Route path='/login' component={LoginForm} />
             <Route path='/register' component={RegisterForm} />
             <Route path='/movies/:id' component={MovieForm} />
--- src/components/common/registerForm.jsx
+++ src/components/common/registerForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Joi from 'joi-browser';
 import Form from './form';
+import { register } from '../../services/userService';
 
 class RegisterForm extends Form {
   state = {
@@ -18,9 +19,18 @@ class RegisterForm extends Form {
     name: Joi.string().required().label('Name')
   };
 
-  doSubmit = () => {
-    // Would normally call server here
-    console.log("Registered!");
+  doSubmit = async () => {
+    try {
+      const response = await register(this.state.data);
+      localStorage.setItem('token', response.headers['x-auth-token']);
+      window.location('/');
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = {...this.state.errors};
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      };
+    };
   };
 
   render() { 
